f1c0c8a97737f0fbcb38cc840ee62e534ae1eb81
DarkKnight <shuklashashwat11@gmail.com>
2021-06-08 12:46:11 +0530

    added START

--- app.js
+++ app.js
@@ -125,6 +125,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Started Port 3000");
+const port = process.env.PORT || 3000 ;
+
+app.listen( port , () => {
+  console.log("Started Port " + port );
 });
